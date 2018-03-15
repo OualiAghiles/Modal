4b98e380f7218a7dcf9e881faa2ee7046fa1121c
Unknown <ouali.aghiles@gmail.com>
2018-03-15 01:36:43 +0100

    wip

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,26 +1,30 @@
-function modal(nscModal) {
+function myModal(nscModal) {
   // btn Modal
   var modalBtn = document.querySelectorAll('.link')
-  modalBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    var target = this.getAttribute('data-target')
-    var modal = document.querySelector(nscModal + '[data-title=' + target + ']')
-    console.log(modal);
-    modal.classList.add('is-visible')
-    modal.addEventListener('click', function (event) {
-      var modalBody =  document.querySelector('.modal-body')
-      if (event.target === modalBody) {
-        modal.classList.remove('is-visible')
-      }
-      event.stopPropagation()
-      event.preventDefault()
-    }, false)
-    close()
-  })
+  for (var i = 0; i < modalBtn.length; i++) {
+    modalBtn[i].addEventListener('click', function (e) {
+      e.preventDefault()
+      var target = this.getAttribute('data-target')
+      var modal = document.querySelector(nscModal + '[data-title=' + target + ']')
+      modal.classList.add('is-visible')
+      console.log('modal', modal)
+      modal.addEventListener('click', function (event) {
+        console.log('modal', modal);
+        var modalBody =  document.querySelector('.modal-body')
+        if (event.target === modalBody) {
+          event.target.parentNode.classList.remove('is-visible')
+        }
+        event.stopPropagation()
+        event.preventDefault()
+      }, false)
+      close()
+    })
+  }
   function close() {
     var closeBtn = document.querySelector(nscModal + ' ' + '.close')
     closeBtn.addEventListener('click', function () {
-      this.parentNode.parentNode.parentNode.classList.remove('is-visible')
+      var modal = document.querySelector(nscModal + '.is-visible')
+      modal.classList.remove('is-visible')
     })
   }
   document.addEventListener('keypress', function (e) {
@@ -30,5 +34,5 @@ function modal(nscModal) {
     }
   })
 }
-modal('.mod1')
-modal('.mod')
+myModal('.mod')
+myModal('.modals')
